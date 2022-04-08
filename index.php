641ce59a5cdba114f35c232a594fd055f4a1b1ce
Odenilson Marques <odenilson.marques@gmz.solutions>
2022-04-08 18:13:26 -0300

    cricao dos cards na tela inicial

--- index.php
+++ index.php
@@ -15,74 +15,38 @@
     <section>
         <div class="container">
             <div class="row mt-5">
-                <div class="col-lg-4 text-center">
-                    <h3>Empresas</h3>
-                    <?php
-                         $seachCompanys = $connectionPDO->prepare("SELECT COUNT(id_company) as registers FROM company");
-                         $seachCompanys->execute();
-                         $rows = $seachCompanys->fetch(PDO::FETCH_ASSOC);
-                         echo $rows['registers'];
-                    ?>
-                </div>
-                <div class="col-lg-4 text-center">
-                    <h3>Licenças</h3>
-                    <?php
-                        $seachLicenses = $connectionPDO->prepare("SELECT COUNT(id_license) as registers FROM license");
-                        $seachLicenses->execute();
-                        $rows = $seachLicenses->fetch(PDO::FETCH_ASSOC);
-                        echo $rows['registers'];
-                    ?>
-                </div>
-                <div class="col-lg-4 text-center">
-                    <h3>Licenças</h3>
-                    <?php
-
-
-                    $listLicenses = [];
-                    $searchLicensesCompanys = $connectionPDO->query("SELECT company.id_company, company.nameCompany, license.id_license, license.type_license, license.expiration_date, license.activity, license.organ FROM license, company WHERE license.id_company = company.id_company");
-                    if($searchLicensesCompanys->rowCount() > 0){
-                    $listLicenses = $searchLicensesCompanys->fetchAll(PDO::FETCH_ASSOC);
+                <div class="col-lg-6 text-center">
+                    <div class="card text-white bg-dark mb-3">
                     
-                    foreach($listLicenses as $listLicense){
-                        // echo $listLicense['nameCompany'];
-                        // echo $listLicense =  strtotime(['expiration_date']);
-                        $expirationDate = date('d/m/Y', strtotime($listLicense['expiration_date']));
-                        // echo "<li id=' $expirationDate'> $expirationDate</li>";
+                        <div class="card-body">
+                            <h5 class="card-title">Empresas</h5>
+                            <?php
+                                $seachCompanys = $connectionPDO->prepare("SELECT COUNT(id_company) as registers FROM company");
+                                $seachCompanys->execute();
+                                $rows = $seachCompanys->fetch(PDO::FETCH_ASSOC); 
+                            ?>
+                                <p style="font-size:30px"><?php echo $rows['registers']; ?></p>
+                        </div>
+                    </div>
+                </div>
 
-                        $expirationDate= strtotime($listLicense['expiration_date']);
-
-                         $days=ceil(($expirationDate-time())/60/60/24);
-                        echo "<li>$days</li>";
-
-                    }
-
-                    }
-            
-                    //capturando os dias de validade das licenças                                          
-                    $expirationDate = strtotime($listLicense['expiration_date']);
-
-                    // $typeLicense = $['type_license'];
-                    // echo $days=ceil(($expirationDate-time())/60/60/24);
-                    // echo "<li id='$days'>$days</li>";
-                    $dataAtual = date('d/m/Y');
-                
-
-
-
-                        $seachLicenses = $connectionPDO->prepare("SELECT COUNT(id_license) as registers FROM license WHERE $days < 0 ");
-                        $seachLicenses->execute();
-                        $rows = $seachLicenses->fetch(PDO::FETCH_ASSOC);
-                        echo $rows['registers'];
-                    ?>
+                <div class="col-lg-6 text-center">
+                    <div class="card text-white bg-dark mb-3" >
+                        
+                        <div class="card-body">
+                            <h5 class="card-title">Licenças</h5>
+                            <?php
+                                $seachCompanys = $connectionPDO->prepare("SELECT COUNT(id_license) as registers FROM license");
+                                $seachCompanys->execute();
+                                $rows = $seachCompanys->fetch(PDO::FETCH_ASSOC); 
+                            ?>
+                                <p style="font-size:30px"><?php echo $rows['registers']; ?></p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        
     </section>
-
-    
-
-
     <?php include_once 'footer.php'?>
     <script src="assets/js/bootstrap.bundle.min.js"></script>
 </body>
--- teste.php
+++ teste.php
@@ -20,3 +20,30 @@ if($days >= 30 && $listLicense['type_license'] == 'LICENÇA CORPO DE BOMBEIRO'){
                                             <?php } elseif($days > 140 ){?>
                                                 <td style="background-color:#198754;color:#FFF;text-align:center;font-size:13px"><strong>DENTRO DO PRAZO</strong></td>
                                             <?php }?>
+
+
+
+                                            <!-- <div class="col-lg-4 text-center">
+                    <h3>Licenças</h3>
+                        <?php
+                        $listLicenses = [];
+                        $searchLicensesCompanys = $connectionPDO->query("SELECT company.id_company, license.id_license, license.expiration_date FROM license, company WHERE license.id_company = company.id_company ");
+                        if($searchLicensesCompanys->rowCount() > 0){
+                            $listLicenses = $searchLicensesCompanys->fetchAll(PDO::FETCH_ASSOC);
+                            foreach($listLicenses as $listLicense){
+                                $expirationDate = strtotime($listLicense['expiration_date']);
+                                $days=ceil(($expirationDate-time())/60/60/24);
+                                $con = 0;
+                                echo "<li>$days</li>"; 
+                                if($days > 120){
+                                echo  $tetet = $days + $con;
+                                }
+                            }
+                        }
+                        $seachLicenses = $connectionPDO->prepare("SELECT sum(expiration_date) as registers FROM license WHERE $days > 120");
+                        $seachLicenses->execute();
+                        
+                        $rows = $seachLicenses->fetch(PDO::FETCH_ASSOC);
+                        echo "etset".$rows['registers'];
+                    ?>
+                </div> -->
