9ad723ef0a2132032ae4873c1ab1675f52c0999d
Odenilson Marques <odenilson.marques@gmz.solutions>
2022-05-23 11:59:41 -0300

    insercao do titulo ao passar o mouse por cima da logo do desenvolvedor

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
     <div class="container">
         <dvi class="row  mt-5">
             <div class="col-lg-12">
-                <p class="text-center">2ps.com</p>
+                <p class="text-center" title="Problemas precisam de soluções">2ps.com</p>
             </div>
         </dvi>
     </div>
